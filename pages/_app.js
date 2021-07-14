43d50b4e9e6d5780e9c98dee2e0a773739bd14e5
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-07-14 00:20:38 -0300

    update day 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../pages/src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
 {
@@ -11,11 +12,25 @@ body {
     background-color: #D9E6F6;
     font-family: sans-serif;
   }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'red',
   },
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,65 @@
 import styled from 'styled-components'
+import MainGrid from '../pages/src/components/MainGrid'
+import Box from '../pages/src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../pages/src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../pages/src/components/ProfileRelations';
 
-/* const Title = styled.h1`
-   font-size: 50px;
-   color: ${({ theme }) => theme.colors.primary};
-`*/
-
-const Box = styled.div`
-    background: #FFFFFF;
-    border-radius: 8px;
-    `;
-
-const MainGrid = styled.main`
-  display: grid;
-  grid-gap: 10px;
-
-`;
+function ProfileSidebar(propriedades) {
+  console.log(propriedades);
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
+  const usuarioAleatorio = 'VictorMacabu' ;
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'filipedeschamps',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'omariosouto'
+  ]
   return (
-  <MainGrid>
-    <Box>
-     Imagem  
-    </Box>
-    <Box>
-      Bem Vindo 
-    </Box>
-    <Box>
-      Comunidades
-    </Box>
-  </MainGrid>
+    <>
+    <AlurakutMenu/>
+    <MainGrid>
+    <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={usuarioAleatorio} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a) 
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
   )
 }
